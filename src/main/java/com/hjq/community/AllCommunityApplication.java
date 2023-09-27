f1b90153a42350ffe3ce37e1dc83f97360c4f715
小白 <2136585216@qq.com>
2023-09-27 21:10:27 +0800

    initial project

---
+++ src/main/java/com/hjq/community/AllCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.hjq.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllCommunityApplication.class, args);
+    }
+
+}
